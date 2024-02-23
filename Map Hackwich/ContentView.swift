b576744e1356c6361c1c33615361437ddaf3949c
Umesh Sarma <usarma1200@gmail.com>
2024-02-23 09:04:50 -0600

    Added user location

--- Map Hackwich/ContentView.swift
+++ Map Hackwich/ContentView.swift
@@ -9,27 +9,26 @@ import SwiftUI
 import MapKit
 
 struct ContentView: View {
-    @State private var startPosition = MapCameraPosition.region(
+    @State private var region = MKCoordinateRegion(
+        center: CLLocationCoordinate2D(
+            latitude: 42.15704,
+            longitude: -88.14812),
+        span: MKCoordinateSpan(
+            latitudeDelta: 0.05,
+            longitudeDelta: 0.05)
+    )
+    @StateObject var locationManager = LocationManager()
+    @State private var userTrackingMode: MapUserTrackingMode = .follow
 
-            MKCoordinateRegion(center: CLLocationCoordinate2D(
-
-                latitude: 42.15559,
-
-                longitude: -88.14692),
-
-            span: MKCoordinateSpan(
-
-                latitudeDelta: 0.05,
-
-                longitudeDelta: 0.05)
-
-            )
-        )
     var body: some View {
         VStack {
-            Map(position: $startPosition)
+            Map(
+                coordinateRegion: $region,
+                interactionModes: .all,
+                showsUserLocation: true,
+                userTrackingMode: $userTrackingMode
+            )
         }
-        
     }
 }
 
--- Map Hackwich/LocationManager.swift
+++ Map Hackwich/LocationManager.swift
@@ -6,3 +6,20 @@
 //
 
 import Foundation
+import CoreLocation
+
+class LocationManager: NSObject, CLLocationManagerDelegate, ObservableObject {
+    var locationManager = CLLocationManager()
+    
+    override init() {
+
+            super.init()
+
+            locationManager.delegate = self
+
+            locationManager.requestWhenInUseAuthorization()
+
+            locationManager.startUpdatingLocation()
+
+        }
+}
